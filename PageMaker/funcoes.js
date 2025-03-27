4b202d6d703f15ef7d891771c14b30f193b70d19
Gabriel_Guilhen <gabrielguilhen0@gmail.com>
2025-03-27 14:44:56 -0300

    Update funcoes.js

--- PageMaker/funcoes.js
+++ PageMaker/funcoes.js
@@ -4,39 +4,39 @@ var ctxConteudo;
 var ctxRodape;
 
 function configEstiloCabecalho(){
-    bg = document.getElementById("corFundo").value;
-    corFonte = document.getElementById("corFonte").value;
-    tamFonte = document.getElementById("tamFonte").value;
-    return "#cabecalho{\n background-color:" + bg + ";\n color:" + corFonte + ";\n font-size:" + tamFonte + "pt;\n}\n";
-}
-
+ bg=document.getElementById("corFundo").value;
+ corFonte=document.getElementById("corFonte").value;
+ tamFonte=document.getElementById("tamFonte").value;
+ ctxCabecalho="#cabecalho{\n background-color:"+bg+";\n";
+ ctxCabecalho+=" color:"+corFonte+";\n";
+ ctxCabecalho+=" font-size:"+tamFonte+"pt;\n}\n";
+ return ctxCabecalho;
+ }  
 function configEstiloLinks(){
-    corLink = document.getElementById("corLinks").value;
-    estiloLinks = document.querySelector('input[name="estiloLinks"]:checked').value;
-    let aux = estiloLinks == "0" ? "none" : "underline";
-    return "a{\n color:" + corLink + ";\n text-decoration:" + aux + ";\n}\n";
+    corLink=document.getElementById("corLinks").value;
+    estiloLinks=document.querySelector('input[name="estiloLinks"]:checked').value;
+    ctxLinks="a{\n color:"+corLink+";\n";
+    let aux=estiloLinks=="0"?"none":"underline";
+    ctxLinks+=" text-decoration:"+aux+";\n}\n";
+    return ctxLinks;
 }
 
-function gerarCodigo(){
-    let codeCSS = document.querySelector("#codeCSS");
-    codeCSS.value = configEstiloCabecalho() + configEstiloLinks();
+ function gerarCodigo(){
+    //Cóigo para CSS
+     let codeCSS=document.querySelector("#codeCSS");
+     let css=configEstiloCabecalho();
+     css+=configEstiloLinks();
+     codeCSS.value=css;
+    //Código para HTML
+     let codeHTML=document.querySelector("#codeHTML");
+     ctxHTML="<html>\n<head>\n" +
+         "<link rel='stylesheet' href='estilo.css'>\n"+
+         "<title>Miha página</title>\n"+
+         "</head>\n<body>" +
+         "<div id='cabecalho'>"+configHTMLCabecalho()+"</div>\n" +
+         "<nav id='links'>\n"+configHtmlLinks()+"\n</nav>\n" +
+         "<div id='conteudo'></div>\n" +
+         "</body>\n</html>";
+     codeHTML.value=ctxHTML;
 
-    let codeHTML = document.querySelector("#codeHTML");
-    let textoCabecalho = document.getElementById("textoCabecalho").value;
-    let textoLinks = document.getElementById("links").value;
-    let arrayLinks = textoLinks.split(";");
-    let linksHTML = "";
-
-    for (let i = 0; i < arrayLinks.length; i++) {
-        linksHTML += "<a href='#'>" + arrayLinks[i] + "</a><br>";
-    }
-
-    codeHTML.value = "<html>\n<head>\n" +
-        "<link rel='stylesheet' href='estilo.css'>\n" +
-        "<title>Minha página</title>\n" +
-        "</head>\n<body>\n" +
-        "<div id='cabecalho'><h1>" + textoCabecalho + "</h1></div>\n" +
-        "<nav id='links'>\n" + linksHTML + "\n</nav>\n" +
-        "<div id='conteudo'></div>\n" +
-        "</body>\n</html>";
-}
+ }
